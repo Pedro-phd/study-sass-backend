aa766eb0a1319cdc637f08fb37db86764b3f8673
Pedro-phd <pedro_dominici@outlook.com>
2025-03-13 12:17:57 -0300

    altera rota de usuários para retornar mensagem fixa

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -3,7 +3,6 @@ import { FastifyInstance } from "fastify";
 export const userRoutes = async (server: FastifyInstance) => {
 
   server.get('/', {}, async (request, reply) => {
-    const users = await server.prisma.user.findMany();
-    return reply.send(users);
+    return reply.send('opa');
   });
 };
